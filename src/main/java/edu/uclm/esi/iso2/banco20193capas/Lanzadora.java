7ded1e38a18e02e4c70d66a08c97753e76e06cc7
alber <alber@DESKTOP-74PAG30>
2020-11-13 09:53:02 +0100

    sonar 2

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/Lanzadora.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/Lanzadora.java
@@ -11,7 +11,7 @@ import edu.uclm.esi.iso2.banco20193capas.model.Cuenta;
 public class Lanzadora {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println();
+		System.out.println("a");
 		SpringApplication.run(Lanzadora.class, args);
 		
 		try {
